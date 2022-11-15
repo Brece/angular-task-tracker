c5e6ee1ffe33a813d1683890a7114053529cbff0
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-11-15 23:05:07 +0100

    adds addTask functionality

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -20,6 +20,10 @@ export class TasksComponent implements OnInit {
     this.taskService.deleteTask(task).subscribe(() => this.tasks = this.tasks.filter(t => t.id !== task.id));
   }
 
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+  }
+
   toggleReminder(task: Task) {
     task.reminder = !task.reminder;
     this.taskService.updateTaskReminder(task).subscribe();
